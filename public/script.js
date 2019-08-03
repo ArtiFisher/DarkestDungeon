c83b1137b6535cd0dabfcfc80b544a3c074f7718
ArtiFisher <artemis.fisher@gmail.com>
2019-08-03 22:50:43 +0300

    bloody form

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ window.auth = {
 
 window.players = {
     drawList: () => {
-        firebase.firestore().collection("participants").get().then(function(querySnapshot) {
+        firebase.firestore().collection("participants").orderBy("score", "desc").get().then(function(querySnapshot) {
             var listFragment = document.createDocumentFragment();
             querySnapshot.forEach(function(doc) {
                 console.log(doc.id, " => ", doc.data());
@@ -16,9 +16,32 @@ window.players = {
                 listItem.innerHTML = `<span>${data.name}</span><span>${data.score}</span>`;
                 listFragment.appendChild(listItem);
             });
+            participants.innerHTML ='';
             participants.appendChild(listFragment);
         })
     }
 }
 
 window.players.drawList();
+addParticipant.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+firebase.auth().onAuthStateChanged((user) => addParticipant.style.display = user ? '' : 'none');
+
+addForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var inputs = Array.from(addForm.elements)
+        .filter(elem => elem.localName == 'input');
+    var values = inputs.map(input => input.value);
+    firebase.firestore().collection("participants").add({
+        name: values[0],
+        score: values[1],
+        phone: values[2],
+        email: values[3]
+    })
+        .then(() => console.info("Added", values))
+        .catch(console.error);
+    inputs.forEach(input => input.value = '');
+    modal.style.display = 'none';
+});
