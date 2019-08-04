673694aaa6028a6ac650903c4a1ab7692af1a3a4
ArtiFisher <artemis.fisher@gmail.com>
2019-08-04 13:54:57 +0300

    SignIn

--- public/script.js
+++ public/script.js
@@ -1,12 +1,18 @@
-window.auth = {
-    getCurrentUser: () => firebase.auth().currentUser,
-    signOut: () => firebase.auth().signOut().then(() => console.info('Sign-out successful')).catch(console.error),
-    signIn: (email, password) => firebase.auth().signInWithEmailAndPassword(email, password).then(() => console.info('Sign-in successful')).catch(console.error),
-    updatePassword: (newPassword) =>  this.auth.getCurrentUser().updatePassword(newPassword).then(() => console.info('Update successful')).catch(console.error)
-}
+window.app = new (class App {
+    constructor() {
+        this.drawList();
+        addParticipant.addEventListener('click', () => this.openForm());
+        cancelForm.addEventListener('click', () => this.closeForm());
+        signIn.addEventListener('click', () => this.getCurrentUser() ? this.signOut() : this.openSignIn());
+        cancelSignIn.addEventListener('click', () => this.closeSignIn());
+        addForm.addEventListener('submit', (event) => this.submitForm(event));
+        signInForm.addEventListener('submit', (event) => this.submitSignIn(event));
+        firebase.auth().onAuthStateChanged((user) => addParticipant.style.display = user ? '' : 'none');
+        this.addInputs = Array.from(addForm.elements).filter(elem => elem.localName == 'input');
+        this.signInInputs = Array.from(signInForm.elements).filter(elem => elem.localName == 'input');
+    }
 
-window.players = {
-    drawList: () => {
+    drawList() {
         firebase.firestore().collection("participants").orderBy("score", "desc").get().then(function(querySnapshot) {
             var listFragment = document.createDocumentFragment();
             querySnapshot.forEach(function(doc) {
@@ -20,28 +26,50 @@ window.players = {
             participants.appendChild(listFragment);
         })
     }
-}
 
-window.players.drawList();
-addParticipant.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+    getCurrentUser() {
+        return firebase.auth().currentUser;
+    }
+    signOut() {
+        firebase.auth().signOut().then(() => console.info('Sign-out successful')).catch(console.error);
+    }
+    signIn(email, password) {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(() => console.info('Sign-in successful')).catch(console.error)
+    }
+    updatePassword(newPassword) {
+        this.getCurrentUser().updatePassword(newPassword).then(() => console.info('Update successful')).catch(console.error)
+    }
 
-firebase.auth().onAuthStateChanged((user) => addParticipant.style.display = user ? '' : 'none');
+    openSignIn() {
+        signInContainer.style.display = 'block';
+    }
+    closeSignIn() {
+        signInContainer.style.display = 'none';
+    }
+    submitSignIn(event) {
+        event.preventDefault();
+        this.signIn(...this.signInInputs.map(({value}) => value));
+        this.closeSignIn();
+    }
 
-addForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var inputs = Array.from(addForm.elements)
-        .filter(elem => elem.localName == 'input');
-    var values = inputs.map(input => input.value);
-    firebase.firestore().collection("participants").add({
-        name: values[0],
-        score: values[1],
-        phone: values[2],
-        email: values[3]
-    })
-        .then(() => console.info("Added", values))
-        .catch(console.error);
-    inputs.forEach(input => input.value = '');
-    modal.style.display = 'none';
-});
+    openForm() {
+        formContainer.style.display = 'block';
+    }
+    closeForm() {
+        this.addInputs.forEach(input => input.value = '');
+        formContainer.style.display = 'none';
+    }
+    submitForm(event) {
+        event.preventDefault();
+        var values = this.addInputs.map(({value}) => value);
+        firebase.firestore().collection("participants").add({
+            name: values[0],
+            score: parseFloat(values[1]),
+            phone: values[2],
+            email: values[3]
+        })
+            .then(() => console.info("Added", values))
+            .catch(console.error);
+        this.closeForm();
+    }
+})();
