dddebd22b1db4fc8d2245748781cd33549c6a282
ArtiFisher <artemis.fisher@gmail.com>
2019-08-17 16:56:16 +0300

    Added gossip form

--- public/script.js
+++ public/script.js
@@ -3,11 +3,31 @@ window.app = new (class App {
         this.renderHeroesList();
         this.renderGossips();
         addParticipant.addEventListener('click', () => this.openForm());
+        addGossip.addEventListener('click', () => this.openGossipForm());
         cancelForm.addEventListener('click', () => this.closeForm());
+        cancelAddGossip.addEventListener('click', () => this.closeForm());
         signIn.addEventListener('click', () => this.getCurrentUser() ? this.signOut() : this.openSignIn());
         cancelSignIn.addEventListener('click', () => this.closeSignIn());
-        addForm.addEventListener('submit', (event) => this.submitForm(event));
-        signInForm.addEventListener('submit', (event) => this.submitSignIn(event));
+        addForm.addEventListener('submit', event => this.submitForm(event));
+        signInForm.addEventListener('submit', event => this.submitSignIn(event));
+        participants.addEventListener('click', event => {
+            var id = event.target.dataset.participantid;
+            if (id) {
+                firebase.firestore().collection("participants").doc(id).delete().then(() => {
+                    console.log("Participant successfully deleted!");
+                    participants.removeChild(event.target.parentElement);
+                }).catch(console.error);
+            }
+        });
+        gossips.addEventListener('click', event => {
+            var id = event.target.dataset.gossipid;
+            if (id) {
+                firebase.firestore().collection("gossips").doc(id).delete().then(() => {
+                    console.log("Gossip successfully deleted!");
+                    gossips.removeChild(event.target.parentElement);
+                }).catch(console.error);
+            }
+        });
         firebase.auth().onAuthStateChanged((user) => {
             const newDisplayValue = user ? '' : 'none'; 
             addParticipant.style.display = newDisplayValue;
@@ -19,6 +39,7 @@ window.app = new (class App {
 
     renderHeroesList() {
         const App = this;
+        App.participants = {};
         firebase.firestore().collection("participants").get().then(querySnapshot => {
             var listFragment = document.createDocumentFragment();
             var results = [];
@@ -26,7 +47,7 @@ window.app = new (class App {
                 console.log(doc.id, " => ", doc.data());
                 console.log(App);
                 var data = doc.data();
-                results.push({
+                var result = {
                     id: doc.id,
                     name: data.name,
                     portraits: data.heirlooms.portraits,
@@ -34,15 +55,16 @@ window.app = new (class App {
                     deeds: data.heirlooms.deeds,
                     crests: data.heirlooms.crests,
                     score: data.heirlooms.portraits * 4 + data.heirlooms.busts * 3 + data.heirlooms.deeds * 2 + data.heirlooms.crests * 1,
-                })
+                };
+                App.participants[doc.id] = result;
+                results.push(result);
             })
             results.sort((participantA, participantB) => participantB.score - participantA.score)
-            .forEach(function(participant) {
-                var listItem = document.createElement('li');
-                listItem.innerHTML = `<span>${participant.name}</span> - <span>${participant.score}</span> - <img class="heirloom" src="./images/heirlooms/Icon_Portrait.png" alt="Portraits"><span>${participant.portraits}</span> <img class="heirloom" src="./images/heirlooms/Icon_Bust.png" alt="Busts"><span>${participant.busts}</span> <img class="heirloom" src="./images/heirlooms/Icon_Deed.png" alt="Deeds"><span>${participant.deeds}</span> <img class="heirloom" src="./images/heirlooms/Icon_Crest.png" alt="Crests"><span>${participant.crests}</span>`;
-                listFragment.appendChild(listItem);
-            });
-            App.participants = results;
+                .forEach(function(participant) {
+                    var listItem = document.createElement('li');
+                    listItem.innerHTML = `<span>${participant.name}</span> - <span>${participant.score}</span> - <img class="heirloom" src="./images/heirlooms/Icon_Portrait.png" alt="Portraits"><span>${participant.portraits}</span> <img class="heirloom" src="./images/heirlooms/Icon_Bust.png" alt="Busts"><span>${participant.busts}</span> <img class="heirloom" src="./images/heirlooms/Icon_Deed.png" alt="Deeds"><span>${participant.deeds}</span> <img class="heirloom" src="./images/heirlooms/Icon_Crest.png" alt="Crests"><span>${participant.crests}</span><span data-participantId=${participant.id}>X</span>`;
+                    listFragment.appendChild(listItem);
+                });
             participants.innerHTML ='';
             participants.appendChild(listFragment);
         })
@@ -55,10 +77,10 @@ window.app = new (class App {
                 console.log(doc.id, " => ", doc.data());
                 var data = doc.data();
                 var listItem = document.createElement('li');
-                listItem.innerHTML = `<span>${data.msg}</span> <span id="${doc.id}">X</span>`;
+                listItem.innerHTML = `<span>${data.msg}</span> <span data-gossipId="${doc.id}">X</span>`;
                 listFragment.appendChild(listItem);
             })
-            gossips.innerHTML ='';
+            gossips.innerHTML = '';
             gossips.appendChild(listFragment);
         })
     }
@@ -86,6 +108,23 @@ window.app = new (class App {
         event.preventDefault();
         this.signIn(...this.signInInputs.map(({value}) => value));
         this.closeSignIn();
+    }
+
+    openGossipForm = () => {
+        addGossipContainer.style.display = 'block';
+    }
+    closeGossipForm() {
+        addGossipInput.value = '';
+        gossipFormContainer.style.display = 'none';
+    }
+    submitGossipForm(event) {
+        event.preventDefault();
+        firebase.firestore().collection("gossips").add({
+            msg: addGossipInput.value,
+        })
+            .then(() => console.info("Added gossip"))
+            .catch(console.error);
+        this.closeGossipForm();
     }
 
     openForm() {
